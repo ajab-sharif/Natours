e6903331ac8e3c2c10f72574e03a536113651177
sharif <sharif@ajab.sharifulislam.com>
2023-02-03 15:12:43 +0300

    implementing a global error handling middleware

--- app.js
+++ app.js
@@ -14,11 +14,25 @@ app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
 // unhandle route
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server!`
-    });
-    next();
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on this server!`
+    // });
+    // next();
+    // create error 
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
 });
+// error handling middleware 
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || '500';
+    err.status = err.status || 'error';
 
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
 module.exports = app;
