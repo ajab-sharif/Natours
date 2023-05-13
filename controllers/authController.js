57c464abb9e473785e2c27cac2bbce24f7da52d0
sharif <sharif@ajab.sharifulislam.com>
2023-05-14 02:26:14 +0300

    Authorization User Role and permission

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,7 +77,7 @@ exports.protect = catchAysnc(async (req, res, next) => {
 exports.restrictTo = function (...roles) {
     return function (req, res, next) {
         if (!roles.includes(req.user.role)) {
-            return next(new AppError('You do not have permision to perform this action', 403));
+            return next(new AppError('You do not have permission to perform this action', 403));
         }
         next();
     }
