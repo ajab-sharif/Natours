09b5b9a57221c50d912c1b429cbcc2de3a42ecc7
sharif <sharif@ajab.sharifulislam.com>
2023-05-22 03:06:30 +0300

    jwt via cookie & Security Http Headers || rate-Limit complete

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require(`express`);
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require("helmet");
 const tourRoute = require('./routes/tourRouter');
 const userRoute = require('./routes/userRouter');
 const AppError = require('./utlis/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-const app = express();
 console.log(process.env.NODE_ENV);
-////////////////////////////////////// midlewere
-//// logger
+const app = express();
+
+const limiter = rateLimit({
+    max: 10,
+    windowMs: 60 * 60 * 1000, // 1h
+    message: "Too many request from this IP, please try again in an hour!",
+});
+
+// Global middelwere 
+// set security HTTP headers
+app.use(helmet());
+//  Limit request from same Api 
+app.use('/api', limiter);
+// Development Logging 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-// for body reading
-app.use(express.json());
+// for body reading from req.body
+app.use(express.json({ limit: '10kb' }));
 // Route Munting
 app.use('/api/v1/tours', tourRoute);
 app.use('/api/v1/users', userRoute);
--- controllers/authController.js
+++ controllers/authController.js
@@ -6,12 +6,20 @@ const AppError = require('../utlis/appError');
 const catchAysnc = require('../utlis/catchAysnc');
 const sendEmail = require('../utlis/email');
 
+const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true
+};
+if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 const signToken = function (id) {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user.id);
+    // remove password form output
+    user.password = undefined;
+    res.cookie('jwt', token, cookieOptions);
     res.status(statusCode).json({
         status: 'success',
         token,
