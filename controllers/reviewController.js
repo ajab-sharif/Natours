753dabc9f265da5999ae2462c37a5a8f2153aad4
sharif <sharif@ajab.sharifulislam.com>
2023-05-25 06:37:16 +0300

    fixed wrong type

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,8 +3,10 @@ const catchAsync = require("../utlis/catchAysnc");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
     let filter = {};
-    if (req.body.tour) filter = { tour: req.params.tourId };
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
     console.log(filter);
+
     const reviews = await Review.find(filter).select('-__v');
     res.status(200).json({
         status: 'success',
